64678444140418af949918b9c4433723ced2d832
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-10 17:34:40 -0300

    getGenres added

--- client/src/Actions/Actions.js
+++ client/src/Actions/Actions.js
@@ -24,3 +24,10 @@ export function getVideogameDetail(id){
 		return dispatch({type: GET_VIDEOGAME_DETAIL, payload: data.data});
 	}
 }
+
+export function getGenres(){
+	return async function(dispatch){
+		const data = await axios.get(`http://localhost:3001/genres`);
+		return dispatch(data.data);
+	}
+}
