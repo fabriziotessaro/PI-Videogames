18aad060551921794f567dd2957c43704e97da60
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-10 11:51:30 -0300

    axios needs a .data to access to results

--- client/src/Actions/Actions.js
+++ client/src/Actions/Actions.js
@@ -6,14 +6,21 @@ export const GET_VIDEOGAME_DETAIL = "GET_VIDEOGAME_DETAIL";
 
 export function getVideogames(name){
 	return async function(dispatch){
-		const data = await axios.get("http://localhost:3000/videogames");
-		return dispatch({type: GET_VIDEOGAMES, payload: data});
+		var data;
+		if(name){
+
+			data = await axios.get(`http://localhost:3001/videogames?name=${name}`);
+		} 
+		else {
+		 	data = await axios.get("http://localhost:3001/videogames");
+		}
+		return dispatch({type: GET_VIDEOGAMES, payload: data.data});
 	}
 }
 
 export function getVideogameDetail(id){
 	return async function(dispatch){
-		const data = await axios.get(`http://localhost:3000/videogame/${id}`);
-		return dispatch({type: GET_VIDEOGAMES, payload: data});
+		const data = await axios.get(`http://localhost:3001/videogame/${id}`);
+		return dispatch({type: GET_VIDEOGAME_DETAIL, payload: data.data});
 	}
 }
